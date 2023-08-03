cfbe52687d286e861f59d067f2b9368788e03a64
Dima Savchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-08-03 11:59:05 +0300

    Initial commit

---
+++ src/js/swiper.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  parallax: true,
+  speed: 1000,
+
+  keyboard: {
+    enable: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.slider-wrapper__count',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#next',
+    prevEl: '#prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
